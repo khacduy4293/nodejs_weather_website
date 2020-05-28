e7ada17a740d7c96d2d25da633bad310fd4f9fc8
khacduy4293 <you@example.com>
2020-05-29 02:46:00 +0700

    Add high and low to forecast

--- src/app.js
+++ src/app.js
@@ -48,12 +48,12 @@ app.get('/weather', (req, res) => {
             error: 'You must provide an address!'
         })
     }
-    geocode(req.query.address , (error, {latidude, longitude, location } = {} ) => {
+    geocode (req.query.address , (error, {latitude, longitude, location } = {} ) => {
         if (error) {
              return res.send({ error })
         }
  
-        forecast (latidude, longitude, (error, forecastData) => {
+        forecast (latitude, longitude, (error, forecastData) => {
              if (error) {
                  return res.send({ error })
              }
